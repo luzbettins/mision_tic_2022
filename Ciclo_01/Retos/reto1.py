bdda60b3378391dc08bbdf8001ba31edaa57a1cc
Luz Bettin <86738625+luzbettins@users.noreply.github.com>
2021-07-01 11:02:20 -0500

    Update reto1.py

--- Ciclo_01/Retos/reto1.py
+++ Ciclo_01/Retos/reto1.py
@@ -1,7 +1,9 @@
 capitan = int(input("Ingrese el puntaje de UDH del Capitan America :"))
 vision = (capitan * 2)+4
 deadpool = (capitan + vision)//5
+
 print(capitan, "", vision, "", deadpool)
+
 if deadpool in range(0, 20):
     print("uno")
 if deadpool in range(21, 30):
